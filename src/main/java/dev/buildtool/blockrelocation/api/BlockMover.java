79b41c66047ec47dddf6e3ce78f496dc3ed6cd91
alexiy <alexiy.ov@gmail.com>
2022-06-07 08:50:00 +0300

    Rename

--- src/main/java/dev/buildtool/blockrelocation/api/BlockMover.java
+++ src/main/java/dev/buildtool/blockrelocation/api/BlockMover.java
@@ -70,34 +70,34 @@ public interface BlockMover {
     /**
      * Modified version of {@link Level#setBlock(BlockPos, BlockState, int, int)}
      */
-    static boolean setBlock(Level level, BlockPos p_46605_, BlockState p_46606_, int p_46607_, int p_46608_) {
-        if (level.isOutsideBuildHeight(p_46605_)) {
+    static boolean setBlock(Level level, BlockPos at, BlockState blockState, int flags, int limit) {
+        if (level.isOutsideBuildHeight(at)) {
             return false;
         } else if (!level.isClientSide && level.isDebug()) {
             return false;
         } else {
-            LevelChunk levelchunk = level.getChunkAt(p_46605_);
+            LevelChunk levelchunk = level.getChunkAt(at);
 
-            p_46605_ = p_46605_.immutable(); // Forge - prevent mutable BlockPos leaks
+            at = at.immutable(); // Forge - prevent mutable BlockPos leaks
             net.minecraftforge.common.util.BlockSnapshot blockSnapshot = null;
             if (level.captureBlockSnapshots && !level.isClientSide) {
-                blockSnapshot = net.minecraftforge.common.util.BlockSnapshot.create(level.dimension(), level, p_46605_, p_46607_);
+                blockSnapshot = net.minecraftforge.common.util.BlockSnapshot.create(level.dimension(), level, at, flags);
                 level.capturedBlockSnapshots.add(blockSnapshot);
             }
 
-            BlockState old = level.getBlockState(p_46605_);
-            int oldLight = old.getLightEmission(level, p_46605_);
-            int oldOpacity = old.getLightBlock(level, p_46605_);
+            BlockState old = level.getBlockState(at);
+            int oldLight = old.getLightEmission(level, at);
+            int oldOpacity = old.getLightBlock(level, at);
 
-            BlockState blockstate = setBlockState(levelchunk, p_46605_, p_46606_, (p_46607_ & 64) != 0);
+            BlockState blockstate = setBlockState(levelchunk, at, blockState);
             if (blockstate == null) {
                 if (blockSnapshot != null) level.capturedBlockSnapshots.remove(blockSnapshot);
                 return false;
             } else {
-                BlockState blockstate1 = level.getBlockState(p_46605_);
-                if ((p_46607_ & 128) == 0 && blockstate1 != blockstate && (blockstate1.getLightBlock(level, p_46605_) != oldOpacity || blockstate1.getLightEmission(level, p_46605_) != oldLight || blockstate1.useShapeForLightOcclusion() || blockstate.useShapeForLightOcclusion())) {
+                BlockState blockstate1 = level.getBlockState(at);
+                if ((flags & 128) == 0 && blockstate1 != blockstate && (blockstate1.getLightBlock(level, at) != oldOpacity || blockstate1.getLightEmission(level, at) != oldLight || blockstate1.useShapeForLightOcclusion() || blockstate.useShapeForLightOcclusion())) {
                     level.getProfiler().push("queueCheckLight");
-                    level.getChunkSource().getLightEngine().checkBlock(p_46605_);
+                    level.getChunkSource().getLightEngine().checkBlock(at);
                     level.getProfiler().pop();
                 }
 
@@ -110,45 +110,45 @@ public interface BlockMover {
      * Modified version of {@link LevelChunk#setBlockState(BlockPos, BlockState, boolean)}
      */
     @javax.annotation.Nullable
-    static BlockState setBlockState(LevelChunk chunk, BlockPos p_62865_, BlockState p_62866_, boolean p_62867_) {
-        int i = p_62865_.getY();
+    static BlockState setBlockState(LevelChunk chunk, BlockPos at, BlockState blockState) {
+        int i = at.getY();
         LevelChunkSection levelchunksection = chunk.getSection(chunk.getSectionIndex(i));
         boolean flag = levelchunksection.hasOnlyAir();
-        if (flag && p_62866_.isAir()) {
+        if (flag && blockState.isAir()) {
             return null;
         } else {
-            int j = p_62865_.getX() & 15;
+            int j = at.getX() & 15;
             int k = i & 15;
-            int l = p_62865_.getZ() & 15;
-            BlockState blockstate = levelchunksection.setBlockState(j, k, l, p_62866_);
-            if (blockstate == p_62866_) {
+            int l = at.getZ() & 15;
+            BlockState blockstate = levelchunksection.setBlockState(j, k, l, blockState);
+            if (blockstate == blockState) {
                 return null;
             } else {
-                Block block = p_62866_.getBlock();
-                chunk.heightmaps.get(Heightmap.Types.MOTION_BLOCKING).update(j, i, l, p_62866_);
-                chunk.heightmaps.get(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES).update(j, i, l, p_62866_);
-                chunk.heightmaps.get(Heightmap.Types.OCEAN_FLOOR).update(j, i, l, p_62866_);
-                chunk.heightmaps.get(Heightmap.Types.WORLD_SURFACE).update(j, i, l, p_62866_);
+                Block block = blockState.getBlock();
+                chunk.heightmaps.get(Heightmap.Types.MOTION_BLOCKING).update(j, i, l, blockState);
+                chunk.heightmaps.get(Heightmap.Types.MOTION_BLOCKING_NO_LEAVES).update(j, i, l, blockState);
+                chunk.heightmaps.get(Heightmap.Types.OCEAN_FLOOR).update(j, i, l, blockState);
+                chunk.heightmaps.get(Heightmap.Types.WORLD_SURFACE).update(j, i, l, blockState);
 
                 boolean flag2 = blockstate.hasBlockEntity();
                 if (chunk.getLevel().isClientSide) {
-                    if ((!blockstate.is(block) || !p_62866_.hasBlockEntity()) && flag2) {
-                        chunk.removeBlockEntity(p_62865_);
+                    if ((!blockstate.is(block) || !blockState.hasBlockEntity()) && flag2) {
+                        chunk.removeBlockEntity(at);
                     }
                 }
 
                 if (!levelchunksection.getBlockState(j, k, l).is(block)) {
                     return null;
                 } else {
-                    if (p_62866_.hasBlockEntity()) {
-                        BlockEntity blockentity = chunk.getBlockEntity(p_62865_, LevelChunk.EntityCreationType.CHECK);
+                    if (blockState.hasBlockEntity()) {
+                        BlockEntity blockentity = chunk.getBlockEntity(at, LevelChunk.EntityCreationType.CHECK);
                         if (blockentity == null) {
-                            blockentity = ((EntityBlock) block).newBlockEntity(p_62865_, p_62866_);
+                            blockentity = ((EntityBlock) block).newBlockEntity(at, blockState);
                             if (blockentity != null) {
                                 chunk.addAndRegisterBlockEntity(blockentity);
                             }
                         } else {
-                            blockentity.setBlockState(p_62866_);
+                            blockentity.setBlockState(blockState);
                             chunk.updateBlockEntityTicker(blockentity);
                         }
                     }
