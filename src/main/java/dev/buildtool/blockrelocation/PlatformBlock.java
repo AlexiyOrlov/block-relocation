fcee01d087ab7706125b23b58fc0193f50285c5e
alexiy <alexiy.ov@gmail.com>
2022-06-16 14:58:05 +0300

    If the placer is sneaking, close the side of the platform corresponding to the placer's look direction after placement

--- src/main/java/dev/buildtool/blockrelocation/PlatformBlock.java
+++ src/main/java/dev/buildtool/blockrelocation/PlatformBlock.java
@@ -4,7 +4,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -33,5 +35,20 @@ public class PlatformBlock extends Block implements EntityBlock {
     @Override
     public BlockEntity newBlockEntity(BlockPos p_153215_, BlockState p_153216_) {
         return BlockRelocation.PLATFORM_ENTITY.get().create(p_153215_, p_153216_);
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos pos, BlockState p_49849_, @Nullable LivingEntity livingEntity, ItemStack p_49851_) {
+        if (livingEntity != null && livingEntity.isShiftKeyDown()) {
+            PlatformEntity platformEntity = (PlatformEntity) level.getBlockEntity(pos);
+            if (livingEntity.getXRot() > 45) {
+                platformEntity.toggleSide(Direction.DOWN, platformEntity.openStates);
+
+            } else if (livingEntity.getXRot() < -45) {
+                platformEntity.toggleSide(Direction.UP, platformEntity.openStates);
+            } else {
+                platformEntity.toggleSide(livingEntity.getDirection(), platformEntity.openStates);
+            }
+        }
     }
 }
